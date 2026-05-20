b8d85707e0b722c1e7ed107e389304e99b2c4244
Maru Grabelli <86084587+marugrabelli@users.noreply.github.com>
2026-05-20 14:32:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from google import genai
 # Configuración de la página de Streamlit
 st.set_page_config(page_title="Tolkien Chatbot", page_icon="🧝‍♂️", layout="centered")
 st.title("LOTR 🧝‍♂️")
-st.subheader("Chatbot para Dr.Rulitos")
+st.subheader("Chatbot für Dr. Rulitos")
 
 # Leer la API Key desde los Secrets de Streamlit
 # Nota: Asegurate de que en Secrets esté escrito exactamente como: GEMINI_API_KEY = "tu_clave"
